0e8b6a2275f4bf1ddc45a9bf33548f92f18e6e37
Tharun1305 <tharunbalaji130507@gmail.com>
2026-08-26 09:32:09 +0530

    Time: 2 ms (40.94%) | Memory: 43.6 MB (95.30%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,31 +1,21 @@
-import java.math.BigInteger;
 class Solution {
     public String shortestBeautifulSubstring(String s,int k) {
-        int n=s.length(),l=0,len=0,cnt1=0,minLen=n+1;
-        BigInteger win=BigInteger.ZERO,xMin=null;
-        for(int r=0;r<n;++r){
-            int is1=s.charAt(r)-'0';
-            win=win.shiftLeft(1).or(BigInteger.valueOf(is1));
-            cnt1+=is1;
-            ++len;
-            while(cnt1>k||(cnt1==k&&s.charAt(l)=='0')){
-                win=win.and(BigInteger.ONE.shiftLeft(len-1).subtract(BigInteger.ONE));
-                --len;
-                cnt1-=s.charAt(l)-'0';
-                ++l;
+        String answer="";
+        int left=0,ones=0;
+        for(int right=0;right<s.length();++right){
+            if(s.charAt(right)=='1') ++ones;
+            while(ones>k){
+                if(s.charAt(left)=='1') --ones;
+                ++left;
             }
-
-            if(cnt1==k){
-                if(len<minLen){
-                    minLen=len;
-                    xMin=win;
-                }
-                else if(len==minLen&&win.compareTo(xMin)<0) xMin=win;
+            while(ones==k&&s.charAt(left)=='0') ++left;
+            if(ones==k){
+                String candidate=s.substring(left,right+1);
+                if(answer.isEmpty()||candidate.length()<answer.length()||
+                   candidate.length()==answer.length()&&candidate.compareTo(answer)<0)
+                    answer=candidate;
             }
         }
-        if(xMin==null) return "";
-        StringBuilder ans=new StringBuilder();
-        for(int i=minLen-1;i>=0;--i) ans.append(xMin.testBit(i)?'1':'0');
-        return ans.toString();
+        return answer;
     }
 }

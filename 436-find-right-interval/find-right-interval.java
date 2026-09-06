b3952d0414f80ae0a83848257ff5b599385eeb6a
Tharun1305 <tharunbalaji130507@gmail.com>
2026-09-06 22:35:57 +0530

    Time: 24 ms (38.05%) | Memory: 50.1 MB (71.77%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        int[] result=new int[intervals.length];
+        NavigableMap<Integer,Integer> intervalMap=new TreeMap<>();
+        for(int i=0;i<intervals.length;++i) intervalMap.put(intervals[i][0],i);    
+        for(int i=0;i<intervals.length;++i){
+            Map.Entry<Integer,Integer> entry=intervalMap.ceilingEntry(intervals[i][1]);
+            result[i]=(entry!=null)?entry.getValue():-1;
+        }
+        return result;
+    }
+}

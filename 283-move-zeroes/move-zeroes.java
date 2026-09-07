26f987d964f79b5ddb326e9c4cd7c40e1123120b
Tharun1305 <tharunbalaji130507@gmail.com>
2026-09-07 23:01:48 +0530

    Time: 2 ms (91.80%) | Memory: 48 MB (20.20%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int right = 0,left=0; right < nums.length; ++right) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                ++left;
+            }
+        }        
+    }
+}

4e66a33548caeec1cd3d2974147412fdba68c711
Tharun1305 <tharunbalaji130507@gmail.com>
2026-09-09 10:02:39 +0530

    Time: 1 ms (99.36%) | Memory: 42.4 MB (93.59%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        if (n <= 999) return 0;
+        long totalCommas = 0,start = 1000;
+        while (start <= n) {
+            totalCommas += n - start + 1;
+            start *= 1000;
+        }
+        return totalCommas;
+    }
+}

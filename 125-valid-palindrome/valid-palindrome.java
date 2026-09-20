8a7bfb87df4c5a734cc80a2d1fac8c5865803d86
Tharun1305 <tharunbalaji130507@gmail.com>
2026-09-20 11:53:34 +0530

    Time: 131 ms (5.24%) | Memory: 48 MB (6.64%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.equals(" ")) return true;
+        String str="";
+        for(char ch:s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)) str+=Character.toLowerCase(ch);
+        }
+        int i=0,j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)) return false;
+            ++i;
+            --j;
+        }
+        return true;
+    }
+}
